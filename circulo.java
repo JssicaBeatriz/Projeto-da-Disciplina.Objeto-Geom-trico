170f83bd209e34936a7d7856b63ce13b2de18207
JssicaBeatriz <163209573+JssicaBeatriz@users.noreply.github.com>
2024-11-10 20:49:47 -0300

    Create circulo.java

---
+++ circulo.java
@@ -0,0 +1,16 @@
+public class Circulo extends FiguraGeometrica {
+    private double raio;
+
+    public Circulo(double raio) {
+        this.raio = raio;
+    }
+
+    public double calcularArea() {
+        return Math.PI * Math.pow(raio, 2);
+    }
+
+    
+    public double calcularPerimetro() {
+        return 2 * Math.PI * raio;
+    }
+}
